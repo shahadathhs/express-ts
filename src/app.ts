25e7b726d227c1c29f61e8a8e7ec3d2a5c660855
shahadathhs <shahadathhossensajib732@gmail.com>
2024-11-15 09:11:50 +0600

    feat: express body parser added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from 'express'
 const app = express()
 
+// ** middlewares **
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.text())
+app.use(express.raw())
+
+// ** routes **
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
