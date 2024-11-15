0619130289bd1c6827602a411eb13f7186987c28
shahadathhs <shahadathhossensajib732@gmail.com>
2024-11-15 09:23:33 +0600

    feat: logger middleware added

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,47 @@
-import express, { Request, Response } from 'express'
-const app = express()
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+
+// ** express app **
+const app: Application = express();
+
+// ** parse request body **
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+app.use(express.raw());
+
+// ** cors **
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:4173",
+      "http://localhost:4174",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 // ** middlewares **
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.text())
-app.use(express.raw())
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log("Method: ", req.method);
+  console.log("URL: ", req.url);
+  console.log("Body: ", req.body);
+  console.log("Query: ", req.query);
+  console.log("Params: ", req.params);
+  console.log("Headers: ", req.headers);
+  next();
+};
 
 // ** routes **
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+app.get("/", logger, (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
 
-app.post('/', (req: Request, res: Response) => {
-  console.log(req.body)
-})
+app.post("/", logger, (req: Request, res: Response) => {
+  console.log(req.body);
+});
 
 export default app;
