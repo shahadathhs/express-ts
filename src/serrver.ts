b12c7488e1a93c58f7ec30ed978a42265f26e798
shahadathhs <shahadathhossensajib732@gmail.com>
2024-11-15 08:31:15 +0600

    feat: initial setup completed

---
+++ src/serrver.ts
@@ -0,0 +1,14 @@
+import { Server } from "http"
+import app from "./app"
+
+const port = 3000
+
+let server: Server
+
+async function start() {
+  server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+}
+
+start()
